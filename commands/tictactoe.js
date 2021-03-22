1192d058a0d174634c328aa1eec5a30338cd4220
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-22 14:59:29 -0400

    Create tictactoe.js

---
+++ commands/tictactoe.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+Discord
+
+
+module.exports = {
+	name: 'test',
+  aliases: ['test2', 't'],
+
+	description: '',
+
+  args: false,
+  //args required?
+
+
+
+  guildOnly: true,
+
+  cooldown: 5,
+  //in secs
+
+	execute(message, args) {
+        
+	},
+};
